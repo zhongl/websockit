7731af63f00ff22b9ff9e2781cadd702ccd0cc15
zhongl <zhong.lunfu@gmail.com>
2013-07-17 14:53:15 +0800

    Use json4s.

--- project/WebSocketKitBuild.scala
+++ project/WebSocketKitBuild.scala
@@ -13,12 +13,14 @@ object WebsocketkitBuild extends Build {
     organization := "me.zhongl",
     version := "0.1-SNAPSHOT",
     scalaVersion := "2.10.0",
+    scalacOptions ++= Seq("-unchecked", "-deprecation", "-optimize"),
+    javacOptions ++= Seq("-target", "1.6", "-source", "1.6"),
     libraryDependencies ++= compileDependencies ++ testDependencies
   )
 
   lazy val compileDependencies = Seq(
     "io.netty" % "netty-codec-http" % "4.0.1.Final",
-    "com.jayway.jsonpath" % "json-path" % "0.8.1",
+    "org.json4s" %% "json4s-native" % "3.2.2",
     "ch.qos.logback" % "logback-classic" % "1.0.13",
     "com.twitter" %% "util-collection" % "6.3.7"
   )
